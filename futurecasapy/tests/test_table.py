42d74bc8820cbca3f36667cc2ccccae9ffac6b75
Simon Perkins <simon.perkins@gmail.com>
2021-05-17 15:20:03 +0200

    WIP

--- futurecasapy/tests/test_table.py
+++ futurecasapy/tests/test_table.py
@@ -10,7 +10,6 @@ from futurecasapy.pytable import (Table,
 @pytest.mark.parametrize("ms", ["/home/sperkins/data/WSRT.MS"])
 def test_casa_table_open(ms):
     table = Table(ms, {"option": "auto"}, 5)
-    import pdb; pdb.set_trace()
     assert table._nrows() == 6552
 
 @pytest.mark.parametrize("inputs", [
@@ -38,20 +37,27 @@ def test_string_convert(inputs, string_type):
         raise ValueError(f"Invalid string_type {string_type}")
 
 @pytest.mark.parametrize("inputs", [
-    "Septimus Thripp",
-    "Severus Snape",
-    1,
-    2.0,
-    3.0 + 4.0j,
-    {"hello": "world"},
-    {"hello": {"this": {"brave": {"new": "world"}}}},
-    {"hello": {"this": {"brave": {"new": "world", "with": 456.0, "lots of": 2, "stuff": 3 + 4j}}}},
-    np.array([[0, 1, 2], [3, 4, 5]], np.complex64).T,
+    # "Septimus Thripp",
+    # "Severus Snape",
+    # 1,
+    # 2.0,
+    # 3.0 + 4.0j,
+    # {"hello": "world"},
+    # {"hello": {"this": {"brave": {"new": "world"}}}},
+    # {"hello": {"this": {"brave": {"new": "world", "with": 456.0, "lots of": 2, "stuff": 3 + 4j}}}},
+    # np.array([[0, 1, 2], [3, 4, 5]], np.complex64).T,
     np.array([[0, 1, 2], [3, 4, 5]], np.complex128).T,
 ])
 def test_valueholder_convert(inputs):
     result = valueholder_convert(inputs)
-    assert result == inputs
+    # assert result == inputs
+    print(f"{inputs} {result} done")
     # print(type(result))
     # print(result.shape, inputs.shape, result.dtype, result.data, len(result.data))
 
+
+def test_capsule_base():
+    from futurecasapy.pytable import test_capsule_base
+    result = test_capsule_base(lambda x: print("0xDEADBEEF"))
+    print(result.shape)
+    print(result.base)
